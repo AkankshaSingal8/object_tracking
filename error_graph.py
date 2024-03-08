dd08d25d41a5980420b80d32095193880647cd7d
root <akanksha21008@iiitd.ac.in>
2024-03-08 18:28:06 +0000

    Error graph

---
+++ error_graph.py
@@ -0,0 +1,33 @@
+import os
+from typing import Iterable, Dict
+import tensorflow as tf
+import kerasncp as kncp
+from kerasncp.tf import LTCCell, WiredCfcCell
+from tensorflow import keras
+import numpy as np
+from matplotlib.image import imread
+from tqdm import tqdm
+from PIL import Image
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+
+csv_file_name = "dataset/1/data_out.csv" 
+labels = np.genfromtxt(csv_file_name, delimiter=',', skip_header=1, dtype=np.float32)[:64]
+
+preds_file_name = "./predictions.csv" 
+pred_vals = np.genfromtxt(preds_file_name, delimiter=',', skip_header=1, dtype=np.float32)
+
+error = labels - pred_vals
+fig, axs = plt.subplots(4, 1, figsize=(10, 8), sharex=True)
+
+for i in range(4):
+    axs[i].plot(error[:, i])
+    axs[i].set_title(f'Error in Dimension {i+1}')
+    axs[i].set_ylabel('Error')
+    axs[i].grid(True)
+
+axs[3].set_xlabel('Sample Index')
+
+plt.tight_layout()
+plt.show()
