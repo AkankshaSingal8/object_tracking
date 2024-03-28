887e45afed2711840925f29f1449d898a455fc39
root <akanksha21008@iiitd.ac.in>
2024-03-28 17:48:49 +0000

    updated dataset correctly

--- updating_dataset.py
+++ updating_dataset.py
@@ -27,10 +27,10 @@ for directory in range(1, 11):
  
     #print("labels", labels[-1])
     
-    with open(csv_file_name, 'a', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        for i in range(diff_labels):
-            csvwriter.writerow(labels[-1])
+    # with open(csv_file_name, 'a', newline='') as csvfile:
+    #     csvwriter = csv.writer(csvfile)
+    #     for i in range(diff_labels):
+    #         csvwriter.writerow(labels[-1])
    
 
    
@@ -44,13 +44,13 @@ for directory in range(1, 11):
     else:
         diff = 64 - d_img
     print("diff_labels", diff_labels, " diff", diff, " diff_labels%64", (labels.shape[0] + diff_labels)%64, " diff%64", (n_images + diff)%64)
-    img_file_name = root + "/" + str(directory) +'/Image' + str(n_images) + '.'+ file_ending
-    img = Image.open(img_file_name)
-    for i in range(1, diff + 1):
+    # img_file_name = root + "/" + str(directory) +'/Image' + str(n_images) + '.'+ file_ending
+    # img = Image.open(img_file_name)
+    # for i in range(1, diff + 1):
        
-        new_filename = os.path.join(root,str(directory))
-        fname =  'Image' + str(i + n_images) + '.'+ file_ending
-        file_path = os.path.join(new_filename, fname)
+    #     new_filename = os.path.join(root,str(directory))
+    #     fname =  'Image' + str(i + n_images) + '.'+ file_ending
+    #     file_path = os.path.join(new_filename, fname)
         
-        img.save(file_path)
+    #     img.save(file_path)
         
