9623096919cd6924c591ddd74854c5a73aa3a42a
root <akanksha21008@iiitd.ac.in>
2024-04-01 13:07:17 +0000

    plotting error - not absolute

--- error_graph.py
+++ error_graph.py
@@ -13,12 +13,12 @@ import time
 import matplotlib.pyplot as plt
 
 csv_file_name = "dataset/1/data_out.csv" 
-labels = np.genfromtxt(csv_file_name, delimiter=',', skip_header=1, dtype=np.float32)[:64]
+labels = np.genfromtxt(csv_file_name, delimiter=',', skip_header=1, dtype=np.float32)
 
-preds_file_name = "./predictions.csv" 
+preds_file_name = "./predictions_sliding_window_updated.csv" 
 pred_vals = np.genfromtxt(preds_file_name, delimiter=',', skip_header=1, dtype=np.float32)
 
-error = abs(labels - pred_vals)
+error = labels - pred_vals
 fig, axs = plt.subplots(4, 1, figsize=(10, 8), sharex=True)
 
 for i in range(4):
@@ -35,4 +35,4 @@ plt.show()
 error_df = pd.DataFrame(error)
 
 # Save the DataFrame to a CSV file
-error_df.to_csv("errors_seq64.csv", index=False)
+error_df.to_csv("errors_updated.csv", index=False)
