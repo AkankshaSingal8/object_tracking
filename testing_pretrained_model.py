cefa20e8df217a1a0d677fd8c8f16a655dab3445
root <akanksha21008@iiitd.ac.in>
2024-04-24 11:04:16 +0000

    pretrained model test without finetuning

--- testing_pretrained_model.py
+++ testing_pretrained_model.py
@@ -19,55 +19,70 @@ from tqdm import tqdm
 from PIL import Image
 import pandas as pd
 import time
+from keras_models import generate_ncp_model
+DROPOUT = 0.1
 
-def generate_ncp_model(seq_len,
-                       image_shape,
-                       augmentation_params=None,
-                       batch_size=None,
-                       seed=DEFAULT_NCP_SEED,
-                       single_step: bool = False,
-                       no_norm_layer: bool = False,
-                       ):
-    inputs_image, inputs_value, x = generate_network_trunk(
-        seq_len,
-        image_shape,
-        augmentation_params=augmentation_params,
-        batch_size=batch_size,
-        single_step=single_step,
-        no_norm_layer=no_norm_layer,
-    )
+DEFAULT_NCP_SEED = 22222
 
-    # Setup the network
-    wiring = kncp.wirings.NCP(
-        inter_neurons=18,  # Number of inter neurons
-        command_neurons=12,  # Number of command neurons
-        motor_neurons=4,  # Number of motor neurons
-        sensory_fanout=6,  # How many outgoing synapses has each sensory neuron
-        inter_fanout=4,  # How many outgoing synapses has each inter neuron
-        recurrent_command_synapses=4,  # Now many recurrent synapses are in the
-        # command neuron layer
-        motor_fanin=6,  # How many incoming syanpses has each motor neuron,
-        seed=seed,  # random seed to generate connections between nodes
-    )
+IMAGE_SHAPE = (144, 256, 3)
+IMAGE_SHAPE_CV = (IMAGE_SHAPE[1], IMAGE_SHAPE[0])
 
-    rnn_cell = LTCCell(wiring)
-
-    if single_step:
-        inputs_state = tf.keras.Input(shape=(rnn_cell.state_size,))
-        # wrap output states in list since want output to just be ndarray, not list of 1 el ndarray
-        motor_out, [output_states] = rnn_cell(x, inputs_state)
-        ncp_model = keras.Model([inputs_image, inputs_value, inputs_state], [motor_out, output_states])
-    else:
-        x = keras.layers.RNN(rnn_cell,
-                             batch_input_shape=(batch_size,
-                                                seq_len,
-                                                x.shape[-1]),
-                             return_sequences=True)(x)
-
-        ncp_model = keras.Model([inputs_image, inputs_value], [x])
-
-    return ncp_model
-
+batch_size = None
+seq_len = 64
+augmentation_params = None
+single_step = False
+no_norm_layer = False
+model = generate_ncp_model(seq_len, IMAGE_SHAPE, augmentation_params, batch_size, DEFAULT_NCP_SEED, single_step, no_norm_layer)
 
 model.load_weights('model-ncp-val.hdf5')
 # predictions = model.predict(images)
+
+root = "./dataset/12"
+image_paths = [path for path in sorted(os.listdir(root)) if 'png' in path]
+
+predictions = []
+
+image_sequences = []
+
+for i in range(len(image_paths)):
+    # Load and process the current and next image in the list
+    current_image_path = os.path.join(root, image_paths[i])
+    current_img = Image.open(current_image_path).resize(IMAGE_SHAPE_CV)
+    current_img_array = np.array(current_img)
+
+    # For the first image, just replicate it 64 times
+    if i == 0:
+        seq = np.stack([current_img_array] * 64)
+        seq = np.expand_dims(seq, axis=0)
+        print(seq.shape)
+        image_sequences.append(seq)
+    else:
+        # Append the current image to the last sequence
+        last_seq = image_sequences[-1]
+        new_seq = np.append(last_seq[0][1:], [current_img_array], axis=0)
+        new_seq = np.expand_dims(new_seq, axis=0) 
+        # print(new_seq.shape)
+        image_sequences.append(new_seq)
+
+# Convert the list of sequences into a numpy array for easier handling
+image_sequences_array = np.array(image_sequences)
+print(image_sequences_array.shape) 
+
+times = []
+for seq in image_sequences_array:
+    preds = []
+    with tf.device('/cpu:0'):
+        start = time.time()
+        preds.append(model.predict(seq))
+        end= time.time()
+        times.append(end - start)
+        print(end - start)
+    predictions.append(preds[0][0][63])
+
+
+print("Avg Time: ", sum(times) / len(times))
+print(len(predictions))
+predictions_df = pd.DataFrame(predictions)
+
+# Save the DataFrame to a CSV file
+predictions_df.to_csv("predictions_pretrained_model.csv", index=False)
