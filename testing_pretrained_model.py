fcd4a21625ae6f6a433f6f6aa78da943d22c7da6
root <akanksha21008@iiitd.ac.in>
2024-04-24 12:34:19 +0000

    pretrained without fine tuning for all the datasets

--- testing_pretrained_model.py
+++ testing_pretrained_model.py
@@ -37,52 +37,54 @@ model = generate_ncp_model(seq_len, IMAGE_SHAPE, augmentation_params, batch_size
 model.load_weights('model-ncp-val.hdf5')
 # predictions = model.predict(images)
 
-root = "./dataset/2"
-image_paths = [path for path in sorted(os.listdir(root)) if 'png' in path]
+for directory in range(3, 12):
 
-predictions = []
+    root = f'./dataset/{directory}'
+    image_paths = [path for path in sorted(os.listdir(root)) if 'png' in path]
 
-image_sequences = []
+    predictions = []
 
-for i in range(len(image_paths)):
-    # Load and process the current and next image in the list
-    current_image_path = os.path.join(root, image_paths[i])
-    current_img = Image.open(current_image_path).resize(IMAGE_SHAPE_CV)
-    current_img_array = np.array(current_img)
+    image_sequences = []
 
-    # For the first image, just replicate it 64 times
-    if i == 0:
-        seq = np.stack([current_img_array] * 64)
-        seq = np.expand_dims(seq, axis=0)
-        print(seq.shape)
-        image_sequences.append(seq)
-    else:
-        # Append the current image to the last sequence
-        last_seq = image_sequences[-1]
-        new_seq = np.append(last_seq[0][1:], [current_img_array], axis=0)
-        new_seq = np.expand_dims(new_seq, axis=0) 
-        # print(new_seq.shape)
-        image_sequences.append(new_seq)
+    for i in range(len(image_paths)):
+        # Load and process the current and next image in the list
+        current_image_path = os.path.join(root, image_paths[i])
+        current_img = Image.open(current_image_path).resize(IMAGE_SHAPE_CV)
+        current_img_array = np.array(current_img)
 
-# Convert the list of sequences into a numpy array for easier handling
-image_sequences_array = np.array(image_sequences)
-print(image_sequences_array.shape) 
+        # For the first image, just replicate it 64 times
+        if i == 0:
+            seq = np.stack([current_img_array] * 64)
+            seq = np.expand_dims(seq, axis=0)
+            print(seq.shape)
+            image_sequences.append(seq)
+        else:
+            # Append the current image to the last sequence
+            last_seq = image_sequences[-1]
+            new_seq = np.append(last_seq[0][1:], [current_img_array], axis=0)
+            new_seq = np.expand_dims(new_seq, axis=0) 
+            # print(new_seq.shape)
+            image_sequences.append(new_seq)
 
-times = []
-for seq in image_sequences_array:
-    preds = []
-    with tf.device('/cpu:0'):
-        start = time.time()
-        preds.append(model.predict(seq))
-        end= time.time()
-        times.append(end - start)
-        print(end - start)
-    predictions.append(preds[0][0][63])
+    # Convert the list of sequences into a numpy array for easier handling
+    image_sequences_array = np.array(image_sequences)
+    print(image_sequences_array.shape) 
+
+    times = []
+    for seq in image_sequences_array:
+        preds = []
+        with tf.device('/cpu:0'):
+            start = time.time()
+            preds.append(model.predict(seq))
+            end= time.time()
+            times.append(end - start)
+            print(end - start)
+        predictions.append(preds[0][0][63])
 
 
-print("Avg Time: ", sum(times) / len(times))
-print(len(predictions))
-predictions_df = pd.DataFrame(predictions)
+    print("Avg Time: ", sum(times) / len(times))
+    print(len(predictions))
+    predictions_df = pd.DataFrame(predictions)
 
-# Save the DataFrame to a CSV file
-predictions_df.to_csv("predictions_pretrained_model_data2.csv", index=False)
+    # Save the DataFrame to a CSV file
+    predictions_df.to_csv(f'./PRETRAINED_W/O_FINETUNE/predictions_pretrained_model_data{directory}.csv', index=False)
