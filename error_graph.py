a8a008e4a58aef7c4db9f1b205422f638d84d41e
root <akanksha21008@iiitd.ac.in>
2024-04-24 12:54:58 +0000

    errors pretrained without fine tuned for entire dataset

--- error_graph.py
+++ error_graph.py
@@ -11,29 +11,31 @@ from PIL import Image
 import pandas as pd
 import time
 import matplotlib.pyplot as plt
+import cv2 as cv
 
-csv_file_name = "dataset/12/data_out.csv" 
-labels = np.genfromtxt(csv_file_name, delimiter=',', skip_header=1, dtype=np.float32)
+for directory in range(3, 12):
+    csv_file_name = f'dataset/{directory}/data_out.csv'
+    labels = np.genfromtxt(csv_file_name, delimiter=',', skip_header=1, dtype=np.float32)
 
-preds_file_name = "predictions_pretrained_model.csv" 
-pred_vals = np.genfromtxt(preds_file_name, delimiter=',', skip_header=1, dtype=np.float32)[:861]
+    preds_file_name = f'PRETRAINED_WO_FINETUNE/predictions_pretrained_model_data{directory}.csv'
+    pred_vals = np.genfromtxt(preds_file_name, delimiter=',', skip_header=1, dtype=np.float32)[:len(labels)]
 
-print(len(labels), len(pred_vals))
-error = labels - pred_vals
-fig, axs = plt.subplots(4, 1, figsize=(10, 8), sharex=True)
+    print(len(labels), len(pred_vals))
+    error = labels - pred_vals
+    fig, axs = plt.subplots(4, 1, figsize=(10, 8), sharex=True)
 
-for i in range(4):
-    axs[i].plot(error[:, i])
-    axs[i].set_title(f'Error in Dimension {i+1}')
-    axs[i].set_ylabel('Error')
-    axs[i].grid(True)
+    for i in range(4):
+        axs[i].plot(error[:, i])
+        axs[i].set_title(f'Error in Dimension {i+1}')
+        axs[i].set_ylabel('Error')
+        axs[i].grid(True)
 
-axs[3].set_xlabel('Sequence Length')
+    axs[3].set_xlabel('Sequence Length')
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
 
-error_df = pd.DataFrame(error)
+    error_df = pd.DataFrame(error)
 
-# # Save the DataFrame to a CSV file
-error_df.to_csv("errors_pretrained_model_data2.csv", index=False)
+    # # Save the DataFrame to a CSV file
+    error_df.to_csv(f'PRETRAINED_WO_FINETUNE/errors_pretrained_model_data{directory}.csv', index=False)
