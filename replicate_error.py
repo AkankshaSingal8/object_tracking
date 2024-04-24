d823bf9d66cac51069a335f889484cb1ec8d782b
root <akanksha21008@iiitd.ac.in>
2024-04-24 13:48:12 +0000

    predicted for all trajectory custom model

--- replicate_error.py
+++ replicate_error.py
@@ -16,57 +16,59 @@ IMAGE_SHAPE_CV = (IMAGE_SHAPE[1], IMAGE_SHAPE[0])
 
 with tf.device('/cpu:0'):
     model = tf.keras.models.load_model('model_ssfalse_b64_lr0.0001wscheduler_seqlen64_new_dataset.h5')
-root = "./dataset/12"
-image_paths = [path for path in sorted(os.listdir(root)) if 'png' in path]
-# print(image_paths)
-file_ending = 'png'
 
-# csv_file_name = "dataset/1/data_out.csv" 
-#labels = np.genfromtxt(csv_file_name, delimiter=',', skip_header=1, dtype=np.float32)
-#print("labels", labels)
-predictions = []
+for directory in range(1, 13):
+    root = f"./dataset/{directory}"
+    image_paths = [path for path in sorted(os.listdir(root)) if 'png' in path]
+    # print(image_paths)
+    file_ending = 'png'
 
-image_sequences = []
+    # csv_file_name = "dataset/1/data_out.csv" 
+    #labels = np.genfromtxt(csv_file_name, delimiter=',', skip_header=1, dtype=np.float32)
+    #print("labels", labels)
+    predictions = []
 
-for i in range(len(image_paths)):
-    # Load and process the current and next image in the list
-    current_image_path = os.path.join(root, image_paths[i])
-    current_img = Image.open(current_image_path).resize(IMAGE_SHAPE_CV)
-    current_img_array = np.array(current_img)
+    image_sequences = []
 
-    # For the first image, just replicate it 64 times
-    if i == 0:
-        seq = np.stack([current_img_array] * 64)
-        seq = np.expand_dims(seq, axis=0)
-        print(seq.shape)
-        image_sequences.append(seq)
-    else:
-        # Append the current image to the last sequence
-        last_seq = image_sequences[-1]
-        new_seq = np.append(last_seq[0][1:], [current_img_array], axis=0)
-        new_seq = np.expand_dims(new_seq, axis=0) 
-        # print(new_seq.shape)
-        image_sequences.append(new_seq)
+    for i in range(len(image_paths)):
+        # Load and process the current and next image in the list
+        current_image_path = os.path.join(root, image_paths[i])
+        current_img = Image.open(current_image_path).resize(IMAGE_SHAPE_CV)
+        current_img_array = np.array(current_img)
 
-# Convert the list of sequences into a numpy array for easier handling
-image_sequences_array = np.array(image_sequences)
-print(image_sequences_array.shape) 
+        # For the first image, just replicate it 64 times
+        if i == 0:
+            seq = np.stack([current_img_array] * 64)
+            seq = np.expand_dims(seq, axis=0)
+            print(seq.shape)
+            image_sequences.append(seq)
+        else:
+            # Append the current image to the last sequence
+            last_seq = image_sequences[-1]
+            new_seq = np.append(last_seq[0][1:], [current_img_array], axis=0)
+            new_seq = np.expand_dims(new_seq, axis=0) 
+            # print(new_seq.shape)
+            image_sequences.append(new_seq)
 
-times = []
-for seq in image_sequences_array:
-    preds = []
-    with tf.device('/cpu:0'):
-        start = time.time()
-        preds.append(model.predict(seq))
-        end= time.time()
-        times.append(end - start)
-        print(end - start)
-    predictions.append(preds[0][0][63])
+    # Convert the list of sequences into a numpy array for easier handling
+    image_sequences_array = np.array(image_sequences)
+    print(image_sequences_array.shape) 
+
+    times = []
+    for seq in image_sequences_array:
+        preds = []
+        with tf.device('/cpu:0'):
+            start = time.time()
+            preds.append(model.predict(seq))
+            end= time.time()
+            times.append(end - start)
+            print(end - start)
+        predictions.append(preds[0][0][63])
 
 
-print("Avg Time: ", sum(times) / len(times))
-print(len(predictions))
-predictions_df = pd.DataFrame(predictions)
+    print("Avg Time: ", sum(times) / len(times))
+    print(len(predictions))
+    predictions_df = pd.DataFrame(predictions)
 
-# Save the DataFrame to a CSV file
-predictions_df.to_csv("predictions_test_12.csv", index=False)
+    # Save the DataFrame to a CSV file
+    predictions_df.to_csv(f"CUSTOM_MODEL/predictions_test_data{directory}.csv", index=False)
