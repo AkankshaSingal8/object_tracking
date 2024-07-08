cb57ecb55b776a25ff9d319d32550743009c8fa0
root <akanksha21008@iiitd.ac.in>
2024-07-08 15:46:37 +0000

    Single step true

---
+++ single_step_true.py
@@ -0,0 +1,44 @@
+import argparse
+import copy
+import json
+import os.path
+from enum import Enum
+from typing import Dict, Tuple, Union, Optional, Any
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from PIL import Image
+from typing import List, Iterable, Optional, Union
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import os
+
+from numpy import ndarray
+from tensorflow import keras, Tensor
+from tensorflow.keras.layers import Conv2D
+from tensorflow.python.keras.models import Functional
+from keras_models import generate_ncp_model
+
+
+IMAGE_SHAPE = (144, 256, 3)
+IMAGE_SHAPE_CV = (IMAGE_SHAPE[1], IMAGE_SHAPE[0])
+
+DEFAULT_NCP_SEED = 22222
+
+batch_size = None
+seq_len = 64
+augmentation_params = None
+single_step = True
+no_norm_layer = False
+mymodel = generate_ncp_model(seq_len, IMAGE_SHAPE, augmentation_params, batch_size, DEFAULT_NCP_SEED, single_step, no_norm_layer)
+
+mymodel.load_weights('ncp_model_b64_seq64_lr0.001.h5')
+
+def load_image(image_path):
+    img = Image.open(image_path)
+    img = img.resize(IMAGE_SHAPE_CV)
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = tf.convert_to_tensor(img_array)
+    return img_array
+
