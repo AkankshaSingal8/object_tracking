7c4db7ce79c277c86bcfc748a969154ad3a0b0a1
root <akanksha21008@iiitd.ac.in>
2024-10-04 14:58:10 +0000

    plotted value for difference and std imgs trained model

---
+++ pipeline/custom_loss_function.py
@@ -0,0 +1,138 @@
+import pickle
+
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+import kerasncp as kncp
+
+import os
+from typing import Iterable, Dict
+import tensorflow as tf
+import kerasncp as kncp
+from kerasncp.tf import LTCCell, WiredCfcCell
+from tensorflow import keras
+import numpy as np
+from matplotlib.image import imread
+from tqdm import tqdm
+from PIL import Image
+import pandas as pd
+import time
+from keras_models import generate_ncp_model
+from train_test_loader import get_dataset_multi, get_val_dataset_multi
+
+class InputLoggerCallback(keras.callbacks.Callback):
+    def on_train_batch_begin(self, batch, logs=None):
+        # Get the current batch of inputs
+        inputs = self.model.input
+        # Use `inputs` as needed, e.g., print or store them
+        print(f"Current inputs at batch {batch}: {inputs}")
+
+input_logger = InputLoggerCallback() 
+
+def tlen(dataset):
+    for (ix, _) in enumerate(dataset):
+        pass
+    return ix
+
+
+training_root = "../fly_to_target_dataset/dataset"
+val_root = "../fly_to_target_dataset/test_data"
+DROPOUT = 0.1
+
+DEFAULT_NCP_SEED = 22222
+
+IMAGE_SHAPE = (144, 256, 3)
+IMAGE_SHAPE_CV = (IMAGE_SHAPE[1], IMAGE_SHAPE[0])
+
+goal_image_pth = "pipeline/goal_image.png"
+goal_image = imread(goal_image_pth)
+goal_image = np.array(goal_image)
+goal_image = goal_image / 255.0
+
+# Custom MSE loss function for two images
+def custom_loss_function(true_data, predicted_data):
+    true_image, true_velocity = true_data
+    predicted_velocity = predicted_data
+    
+    # Calculate MSE for velocity (between true and predicted)
+    mse_velocity = tf.reduce_mean(tf.square(true_velocity - predicted_velocity))
+    
+    # Calculate MSE between input image and goal image
+    mse_image = tf.reduce_mean(tf.square(true_image - goal_image))
+    
+    # Combine both losses
+    total_loss = mse_velocity + mse_image
+    return total_loss
+    
+
+    
+
+batch_size = None
+seq_len = 64
+augmentation_params = None
+single_step = False
+no_norm_layer = False
+
+decay_rate: float = 0.85
+lr: float = 0.001
+lr_schedule = keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=lr, decay_steps=500,
+                                                            decay_rate=decay_rate, staircase=True)
+#Adam optimizer
+optimizer = keras.optimizers.Adam(learning_rate=lr_schedule)
+
+gpus = tf.config.list_logical_devices('GPU')
+strategy = tf.distribute.MirroredStrategy(gpus)
+with strategy.scope():
+    mymodel = generate_ncp_model(seq_len, IMAGE_SHAPE, augmentation_params, batch_size, DEFAULT_NCP_SEED, single_step, no_norm_layer)
+    mymodel.compile(optimizer=optimizer, loss=custom_loss_function, metrics=['mse'])
+    mymodel.load_weights('saved_models/fine_tuned_woscheduler_seed22222_lr0.0001_trainloss0.00012_valloss0.08719_diff_dataset.h5')
+
+    mymodel.summary()
+
+# shift: int = 1
+# stride: int = 1
+# # decay_rate: float = 0.95
+# val_split: float = 0.2
+# label_scale: float = 1
+# seq_len = 64
+# val_split: float = 0.1
+# label_scale: float = 1
+
+# with tf.device('/cpu:0'):
+#     training_dataset = get_dataset_multi(training_root, IMAGE_SHAPE, seq_len, shift, stride, val_split, label_scale, extra_data_root=None)
+#     val_data = get_val_dataset_multi(val_root, IMAGE_SHAPE, seq_len, shift, stride, val_split, label_scale, extra_data_root=None)
+
+# print('\n\nTraining Dataset Size: %d\n\n' % tlen(training_dataset))
+
+# print('load dataset shape', training_dataset.element_spec)
+# training_dataset = training_dataset.shuffle(100).batch(64)
+
+# val_dataset = val_data.batch(64)
+# print('load val dataset shape', val_dataset.element_spec)
+
+# options = tf.data.Options()
+# options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
+# training_dataset = training_dataset.with_options(options)
+# validation_dataset = val_dataset.with_options(options)
+# # Have GPU prefetch next training batch while first one runs
+# training_dataset = training_dataset.prefetch(tf.data.AUTOTUNE)
+# validation_dataset = validation_dataset.prefetch(tf.data.AUTOTUNE)
+
+
+# epochs: int = 1
+
+# #setting validation data to None
+# history = mymodel.fit(x=training_dataset, validation_data=val_dataset, epochs=epochs,verbose=1, use_multiprocessing=False, workers=1, max_queue_size=5, callbacks=[input_logger])
+# print(history)
+
+# # Extract the final training and validation loss
+# train_loss = history.history['loss'][-1]
+# val_loss = history.history['val_loss'][-1]
+
+
+# accuracy = mymodel.evaluate(x=training_dataset)
+# print('Accuracy:' ,accuracy)
+
+# mymodel.save(f'saved_models/custom_loss.h5')
