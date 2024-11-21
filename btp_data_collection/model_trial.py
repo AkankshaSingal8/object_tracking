7d1809feb7552a2979a066fe5a0b7a155264edd4
Akanksha Singal <108049032+AkankshaSingal8@users.noreply.github.com>
2024-11-21 19:23:00 +0530

    updated python2 and python3 bridge

--- btp_data_collection/model_trial.py
+++ btp_data_collection/model_trial.py
@@ -1,10 +1,7 @@
-import json
-
 def main():
     # Example list of values to return
     result = [1, 2, 3, 4, 5]
-    # Print the list as a JSON string
-    print(json.dumps(result))
+    print(result)
 
 if __name__ == "__main__":
     main()
